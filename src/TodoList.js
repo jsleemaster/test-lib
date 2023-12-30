5c69912c9b08221e876a7719860e3c90e9568854
jsleemaster <roeowo123@naver.com>
2023-12-30 15:21:32 +0900

    TodoApp 테스트 추가

--- src/TodoList.js
+++ src/TodoList.js
@@ -4,7 +4,7 @@ import TodoItem from './TodoItem';
 
 const TodoList = ({ todos, onToggle, onRemove }) => {
 
-  return <ul>
+  return <ul data-testid="TodoList">
     {todos.map(todo => (
       <TodoItem todo={todo} key={todo.id} onToggle={onToggle} onRemove={onRemove} />
     ))}
