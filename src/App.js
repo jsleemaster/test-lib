806e4b1e1858485813fcfa298f78435105eef104
jsleemaster <roeowo123@naver.com>
2023-12-30 14:21:18 +0900

    react 환경에서 TDD 만들기

---
+++ src/App.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import Profile from './Profile';
+
+const App = () => {
+  return <Profile username="userName" name="테스트" />;
+};
+
+export default App;
---
+++ src/Profile.js
@@ -0,0 +1,12 @@
+import react from "react";
+
+const Profile = ({ username, name }) => {
+  return (
+    <div>
+      <b>{username}</b>&nbsp;
+      <span>({name})</span>
+    </div>
+  );
+};
+
+export default Profile;
